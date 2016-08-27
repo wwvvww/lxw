b14c987253191cbf20c3b026c87da39d313ef504
wwvvww <www@zhiv.net>
2016-08-27 08:06:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 echo "Hello,world!<br/>";
 echo "My first github page,feel verywell!";
-echo "<a href="./sqlite.php">数据库测试</a>";
+echo "<a href="http:\/\/lxw.herokuapp.com/sqlite.php">数据库测试<\/a>";
 ?>
