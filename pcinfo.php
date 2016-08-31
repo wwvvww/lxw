8cf6fe9a56b32a496697477baf6b384202e13805
wwvvww <www@zhiv.net>
2016-08-31 08:45:53 +0800

    Update pcinfo.php

--- pcinfo.php
+++ pcinfo.php
@@ -1,22 +1,128 @@
 <?php
-exec("SYSTEMINFO",$result);
-
-//数组写入文本文件 
-$time = time();
-//使用fopen("$time.txt",'w');可以以当前时间命令文件,以单引号刚只是变量
-$fp = fopen("$time.txt",'w+');
-
-fwrite($fp,var_export($result,true));
-fclose($fp);
-
-//显示数组获取到的内容，使用FOREACH不用设置循环次数
-foreach($result as $xiangmu)
-    echo $xiangmu."<br/>";
 /**
-for ($i=0;$i<50;$i++)
-    echo "$result[$i].<br/> ";
-**/
-
- //	print_r($result);
- 	
- 	?>
+ * 获取客户端操作系统信息包括win10
+ * @param  null
+ * @author  Jea杨
+ * @return string 
+ */
+function get_os(){
+$agent = $_SERVER['HTTP_USER_AGENT'];
+    $os = false;
+ 
+    if (preg_match('/win/i', $agent) && strpos($agent, '95'))
+    {
+      $os = 'Windows 95';
+    }
+    else if (preg_match('/win 9x/i', $agent) && strpos($agent, '4.90'))
+    {
+      $os = 'Windows ME';
+    }
+    else if (preg_match('/win/i', $agent) && preg_match('/98/i', $agent))
+    {
+      $os = 'Windows 98';
+    }
+    else if (preg_match('/win/i', $agent) && preg_match('/nt 6.0/i', $agent))
+    {
+      $os = 'Windows Vista';
+    }
+    else if (preg_match('/win/i', $agent) && preg_match('/nt 6.1/i', $agent))
+    {
+      $os = 'Windows 7';
+    }
+	  else if (preg_match('/win/i', $agent) && preg_match('/nt 6.2/i', $agent))
+    {
+      $os = 'Windows 8';
+    }else if(preg_match('/win/i', $agent) && preg_match('/nt 10.0/i', $agent))
+    {
+      $os = 'Windows 10';#添加win10判断
+    }else if (preg_match('/win/i', $agent) && preg_match('/nt 5.1/i', $agent))
+    {
+      $os = 'Windows XP';
+    }
+    else if (preg_match('/win/i', $agent) && preg_match('/nt 5/i', $agent))
+    {
+      $os = 'Windows 2000';
+    }
+    else if (preg_match('/win/i', $agent) && preg_match('/nt/i', $agent))
+    {
+      $os = 'Windows NT';
+    }
+    else if (preg_match('/win/i', $agent) && preg_match('/32/i', $agent))
+    {
+      $os = 'Windows 32';
+    }
+    else if (preg_match('/linux/i', $agent))
+    {
+      $os = 'Linux';
+    }
+    else if (preg_match('/unix/i', $agent))
+    {
+      $os = 'Unix';
+    }
+    else if (preg_match('/sun/i', $agent) && preg_match('/os/i', $agent))
+    {
+      $os = 'SunOS';
+    }
+    else if (preg_match('/ibm/i', $agent) && preg_match('/os/i', $agent))
+    {
+      $os = 'IBM OS/2';
+    }
+    else if (preg_match('/Mac/i', $agent) && preg_match('/PC/i', $agent))
+    {
+      $os = 'Macintosh';
+    }
+    else if (preg_match('/PowerPC/i', $agent))
+    {
+      $os = 'PowerPC';
+    }
+    else if (preg_match('/AIX/i', $agent))
+    {
+      $os = 'AIX';
+    }
+    else if (preg_match('/HPUX/i', $agent))
+    {
+      $os = 'HPUX';
+    }
+    else if (preg_match('/NetBSD/i', $agent))
+    {
+      $os = 'NetBSD';
+    }
+    else if (preg_match('/BSD/i', $agent))
+    {
+      $os = 'BSD';
+    }
+    else if (preg_match('/OSF1/i', $agent))
+    {
+      $os = 'OSF1';
+    }
+    else if (preg_match('/IRIX/i', $agent))
+    {
+      $os = 'IRIX';
+    }
+    else if (preg_match('/FreeBSD/i', $agent))
+    {
+      $os = 'FreeBSD';
+    }
+    else if (preg_match('/teleport/i', $agent))
+    {
+      $os = 'teleport';
+    }
+    else if (preg_match('/flashget/i', $agent))
+    {
+      $os = 'flashget';
+    }
+    else if (preg_match('/webzip/i', $agent))
+    {
+      $os = 'webzip';
+    }
+    else if (preg_match('/offline/i', $agent))
+    {
+      $os = 'offline';
+    }
+    else
+    {
+      $os = '未知操作系统';
+    }
+    return $os;  
+}
+?>
