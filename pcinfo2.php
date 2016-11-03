21416be2c210fe0cace17dd797e973343b5cd773
wwvvww <www@zhiv.net>
2016-11-03 10:28:22 +0800

    Update pcinfo2.php

--- pcinfo2.php
+++ pcinfo2.php
@@ -1,5 +1,21 @@
 <?php
-exec("SYSTEMINFO",$result);
+//exec("SYSTEMINFO",$result);
+// 定义一个函数getIP()
+function getIP()
+{
+global $ip;
+if (getenv("HTTP_CLIENT_IP"))
+$ip = getenv("HTTP_CLIENT_IP");
+else if(getenv("HTTP_X_FORWARDED_FOR"))
+$ip = getenv("HTTP_X_FORWARDED_FOR");
+else if(getenv("REMOTE_ADDR"))
+$ip = getenv("REMOTE_ADDR");
+else $ip = "Unknow";
+return $ip;
+}
+ 
+// 使用方法：
+echo getIP();
 
 //数组写入文本文件 
 $time = time();
