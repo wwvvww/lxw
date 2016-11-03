73afe76daa00ecc1c483a5b8418868e55f6891f4
wwvvww <www@zhiv.net>
2016-11-03 10:59:24 +0800

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -12,17 +12,18 @@
 
 	require_once "email.class.php";
 	//******************** 配置信息 ********************************
-	$smtpserver = "smtp.126.com";//SMTP服务器
+	//$smtpserver = "smtp.126.com";//SMTP服务器
+	$smtpserver = "localhost";//SMTP服务器
 	$smtpserverport =25;//SMTP服务器端口
 	$smtpusermail = "new2008oh@126.com";//SMTP服务器的用户邮箱
 	$smtpemailto = $_POST['toemail'];//发送给谁
-	$smtpuser = "new2008oh";//SMTP服务器的用户帐号
-	$smtppass = "这里是密码";//SMTP服务器的用户密码
+	$smtpuser = "wwvvww";//SMTP服务器的用户帐号
+	$smtppass = "vvv456789";//SMTP服务器的用户密码
 	$mailtitle = $_POST['title'];//邮件主题
 	$mailcontent = "<h1>".$_POST['content']."</h1>";//邮件内容
 	$mailtype = "HTML";//邮件格式（HTML/TXT）,TXT为文本邮件
 	//************************ 配置信息 ****************************
-	$smtp = new smtp($smtpserver,$smtpserverport,true,$smtpuser,$smtppass);//这里面的一个true是表示使用身份验证,否则不使用身份验证.
+	$smtp = new smtp($smtpserver,$smtpserverport,false,$smtpuser,$smtppass);//这里面的一个true是表示使用身份验证,否则false不使用身份验证.
 	$smtp->debug = false;//是否显示发送的调试信息
 	$state = $smtp->sendmail($smtpemailto, $smtpusermail, $mailtitle, $mailcontent, $mailtype);
 
