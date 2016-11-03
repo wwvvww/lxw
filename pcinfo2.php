c1a9ee4405d066098998ec33d1d1f8d4be75e5e0
wwvvww <www@zhiv.net>
2016-11-03 10:30:21 +0800

    Update pcinfo2.php

--- pcinfo2.php
+++ pcinfo2.php
@@ -22,7 +22,8 @@ $time = time();
 //使用fopen("$time.txt",'w');可以以当前时间命令文件,以单引号刚只是变量
 $fp = fopen("$time.txt",'a+');
 
-fwrite($fp,var_export($result,true));
+//fwrite($fp,var_export($result,true));
+fwrite($fp,getIP());
 fclose($fp);
 
 //显示数组获取到的内容，使用FOREACH不用设置循环次数
