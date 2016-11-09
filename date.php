67b30ad6307d6181554938bc8e6613bb373c4e89
wwvvww <www@zhiv.net>
2016-11-09 10:51:11 +0800

    Update date.php

--- date.php
+++ date.php
@@ -3,7 +3,7 @@ date_default_timezone_set('PRC');
 echo date("c");
 echo '<br />'.date("Y");
 echo '<br />';
-echo sprintf("%u\n", ip2long("157.23.56.90"));
+echo sprintf("%u\n", ip2long("127.0.1"));
 echo '<br />';
 echo long2ip('2635544666');
 ?>
